9c9f46b15ab3e27c6e67cf24f3fc31854279bfae
AnnRazarenova <anyrazarenova@gmail.com>
2025-12-12 12:45:18 +0700

    все классы добавлены

--- HomeWorkClasses/HomeWorkClasses/Program.cs
+++ HomeWorkClasses/HomeWorkClasses/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace HomeWorkClasses
+﻿using System.Xml.Linq;
+
+namespace HomeWorkClasses
 {
-    internal class Unit
+    public class Unit
     {
         public string Name { get; }
 
         private float health;
         public float Health { get => health; }
 
-        public int Damage { get; }
+        public Interval DamageToPlayer { get; }
 
         public float Armor { get; }
 
@@ -21,7 +23,7 @@
         public Unit(string name)
         {
             Name = name;
-            Damage = 5;
+            DamageToPlayer = new Interval(0, 20);
             Armor = 0.6f;
             health = 100f;
         }
@@ -46,113 +48,211 @@
                 return true;
             }
         }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Характеристики игрока:");
-            Unit unit = new Unit("Igrok");
-            Console.WriteLine("Имя игрока: " + unit.Name);
-            Console.WriteLine("Здоровье игрока: " + unit.Health);
-            Console.WriteLine("Урон игрока: " + unit.Damage);
-            Console.WriteLine("Броня игрока: " + unit.Armor);
-            Console.WriteLine("Здоровье игрока с бронёй: " + unit.GetRealHealth());
-            Console.WriteLine();
-
-            Console.WriteLine("Битва: ");
-            while (unit.Health > 0f)
-            {
-                Console.WriteLine("Игроку нанесён урон в 50 единиц");
-                Console.WriteLine("Игрок жив: " + unit.SetDamage(50));
-                Console.WriteLine("Здоровье игрока: " + Math.Round(unit.Health));
-                Console.WriteLine("Здоровье игрока с бронёй: " + Math.Round(unit.GetRealHealth()));
-                Console.WriteLine();
-            }
-
-            Wepon wepon = new Wepon("Gun", 10, 50);
-
-            Console.WriteLine("Название оружия: " + wepon.Name);
-            Console.WriteLine("Минимальный урон оружия: " + wepon.MinDamage);
-            Console.WriteLine("Максимальный урон оружия: " + wepon.MaxDamage);
-            Console.WriteLine("Прочность оружия: " + wepon.Durability);
-            Console.WriteLine("Средний урон оружия: " + wepon.GetDamage());
-
-
-
-        }
     }
 
-    internal class Wepon
+    public class Weapon
     {
         public string Name { get; }
 
-        public int MinDamage {  get; private set; }
+        //public int MinDamage {  get; private set; }
 
-        public int MaxDamage { get; private set; }
+        //public int MaxDamage { get; private set; }
+
+        public Interval Damage { get; }
 
         public float Durability { get; }
 
-        public Wepon() : this("Unkown wepon", 1, 10)
+        public Weapon() : this("Unkown weapon", new Interval(1, 10))
         {
 
         }
 
-        public Wepon(string name, int minDamage, int maxDamage)
+        public Weapon(string name, Interval damage/*int minDamage, int maxDamage*/)
         {
             Name = name;
-            MinDamage = minDamage;
-            MaxDamage = maxDamage;
+            //MinDamage = minDamage;
+            //MaxDamage = maxDamage;
+            Damage = damage;
             Durability = 1f;
             
-            SetDamageParams (MinDamage, MaxDamage);
+            //SetDamageParams (MinDamage, MaxDamage);
         }
 
-        public void SetDamageParams(int minDamage, int maxDamage)
+        //public void SetDamageParams(int minDamage, int maxDamage)
+        //{
+        //    int f = 1;
+        //    if (minDamage > maxDamage)
+        //    {
+        //        int swap;
+        //        swap = minDamage;
+        //        minDamage = maxDamage;
+        //        maxDamage = swap;
+
+        //        Console.WriteLine("\r\nIncorrect input data for the weapon: " + Name);
+        //    }
+        //    else
+        //    {
+        //        if (minDamage < 1)
+        //        {
+        //            minDamage = f;
+        //            Console.WriteLine($"Forced setting of the minimum value for a weapon: {Name}");
+        //        }
+        //        if (maxDamage <= 1)
+        //        {
+        //            maxDamage = 10;
+        //        }
+        //    }
+
+        //    MinDamage = minDamage;
+        //    MaxDamage = maxDamage;
+
+        //}
+
+        public /*int*/ Interval GetDamage()
         {
-            int f = 1;
-            if (minDamage > maxDamage)
+            return Damage/*(MaxDamage +  MinDamage)/2*/;
+        }
+
+    }
+
+    public struct Interval
+    {
+        public int Min { get; }
+        public int Max { get; }
+
+        Random random = new Random();
+
+        public int Get { get; set; }
+
+        public Interval(int minValue, int maxValue) 
+        {
+            if (minValue > maxValue)
             {
                 int swap;
-                swap = minDamage;
-                minDamage = maxDamage;
-                maxDamage = swap;
+                swap = minValue;
+                minValue = maxValue;
+                maxValue = swap;
 
-                Console.WriteLine("Некорректные входные данные у оружия: " + Name);
+                Console.WriteLine("Incorrect input data");
             }
             else
             {
-                if (minDamage < 1)
+                if (minValue < 0)
                 {
-                    minDamage = f;
-                    Console.WriteLine($"Форсированная установка минимального значения у оружия: {Name}");
+                    minValue = 0;
+                    Console.WriteLine("Incorrect input data");
+
                 }
-                if (maxDamage <= 1)
+
+                if (maxValue < 0)
                 {
-                    maxDamage = 10;
+                    maxValue = 0;
+                    Console.WriteLine("Incorrect input data");
                 }
+
+                if (minValue ==  maxValue)
+                {
+                    maxValue = minValue + 10;
+                    Console.WriteLine("Incorrect input data");
+                }
+
+                
             }
 
-            MinDamage = minDamage;
-            MaxDamage = maxDamage;
+            Min = minValue; 
+            Max = maxValue;
 
-        }
+            Get = random.Next(minValue, maxValue);
+        } 
 
-        public int GetDamage()
+    }
+
+    public struct Room
+    {
+        public Unit Unit;
+
+        public Weapon Weapon;
+
+        public Room(Unit unit, Weapon wepon)
         {
-            return (MaxDamage +  MinDamage)/2;
+            Unit = unit;
+            Weapon = wepon;
+        }
+    }
+
+    public class Dungeon
+    {
+        Room[] rooms;
+
+        public Dungeon()
+        {
+            rooms = new Room[] { 
+                new Room(new Unit("Soldier"), new Weapon("Sword", new Interval(10, 17))), 
+                new Room(new Unit("Poet"), new Weapon("Lira", new Interval(1, 5))), 
+                new Room(new Unit("King"), new Weapon("Magic Sword", new Interval(23, 35))), 
+                new Room(new Unit("Wizard"), new Weapon("Staff", new Interval(2, 7))) };
         }
 
-        //static void Main(string[] args)
-        //{
-        //    Wepon wepon = new Wepon("Gun", 10, 50);
+        public void ShowRooms()
+        {
+            for (int i = 0; i < rooms.Length; i++) // поле типа Room[]
+            {
+                var room = rooms[i];
 
-        //    Console.WriteLine("Название оружия: " + wepon.Name);
-        //    Console.WriteLine("Минимальный урон оружия: " + wepon.MinDamage);
-        //    Console.WriteLine("Максимальный урон оружия: " + wepon.MaxDamage);
-        //    Console.WriteLine("Прочность оружия: " + wepon.Durability);
-        //    Console.WriteLine("Средний урон оружия: " + wepon.GetDamage());
+                Console.WriteLine();
+                Console.WriteLine("Unit of room " + i + ": " + room.Unit.Name);
+                Console.WriteLine("Weapon of room " + i + ": " + room.Weapon.Name);
+                Console.WriteLine("Weapon damage " + i + ": " + room.Weapon.Damage.Get);
+                Console.WriteLine("Health Unit of room " + i + ": " + room.Unit.Health);
+                Console.WriteLine("Damage to Unit of room " + i + ": " + room.Unit.DamageToPlayer.Get);
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------------------");
+            }
+        }
+    }
 
 
-        //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dungeon dungeon = new Dungeon();
+            dungeon.ShowRooms();
+
+            //Console.WriteLine("Player characteristics:");
+            //Unit unit = new Unit("Igrok");
+            //Console.WriteLine("Player name: " + unit.Name);
+            //Console.WriteLine("Player health: " + unit.Health);
+            //Console.WriteLine("Player Damage: " + unit.Damage);
+            //Console.WriteLine("Player armor: " + unit.Armor);
+            //Console.WriteLine("Player health with armor: " + unit.GetRealHealth());
+            //Console.WriteLine();
+
+            //Console.WriteLine("Battle: ");
+            //while (unit.Health > 0f)
+            //{
+            //    Console.WriteLine("The player suffered 50 damage");
+            //    Console.WriteLine("The player is alive: " + unit.SetDamage(50));
+            //    Console.WriteLine("Player health: " + Math.Round(unit.Health));
+            //    Console.WriteLine("Player health with armor: " + Math.Round(unit.GetRealHealth()));
+            //    Console.WriteLine();
+            //}
+
+            //Weapon weapon = new Weapon("Gun", 10, 50);
+
+            //Console.WriteLine("Weapon name: " + weapon.Name);
+            //Console.WriteLine("Minimum weapon damage: " + weapon.MinDamage);
+            //Console.WriteLine("Maximum weapon damage: " + weapon.MaxDamage);
+            //Console.WriteLine("Weapon durability: " + weapon.Durability);
+            //Console.WriteLine("Average weapon damage: " + weapon.GetDamage());
+            
+
+
+
+        
+        
+        }
     }
 
 }
