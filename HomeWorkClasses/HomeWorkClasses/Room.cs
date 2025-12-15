7009fcedc24bcd2fa7c180abe1b53dc2b5012258
Ann <anyrazarenova@gmail.com>
2025-12-15 19:15:36 +0700

    Доработка

---
+++ HomeWorkClasses/HomeWorkClasses/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkClasses
+{
+    public struct Room
+    {
+        public Unit Unit { get; }
+
+        public Weapon Weapon { get; }
+
+        public Room(Unit unit, Weapon wepon)
+        {
+            Unit = unit;
+            Weapon = wepon;
+        }
+    }
+
+}
--- HomeWorkClasses/HomeWorkClasses/obj/Debug/net8.0/HomeWorkClasses.AssemblyInfo.cs
+++ HomeWorkClasses/HomeWorkClasses/obj/Debug/net8.0/HomeWorkClasses.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWorkClasses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb86cf906ec4e55ebc2f5a3dbdf6c675968aaa3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e84ab3970f3e31fb9529abca0dced77d0e577d1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWorkClasses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWorkClasses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
