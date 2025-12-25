ad7b8e371e2e9925425580c57c6fb1fd617a198d
Ann <anyrazarenova@gmail.com>
2025-12-25 19:29:13 +0700

    коммы

--- GamePrototype/obj/Debug/net6.0/GamePrototype.AssemblyInfo.cs
+++ GamePrototype/obj/Debug/net6.0/GamePrototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamePrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4164f3cb31b3c4d3e8cc21823a92062407a41dbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29c850bc237c170d96c97258bac5ba893406cb43")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamePrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamePrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
